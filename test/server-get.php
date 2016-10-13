4572e0b0695731fd1130ab559843e327faf8bcf4
阿狼 <stormte@gmail.com>
2016-10-13 22:36:55 +0800

    删除测试文件

--- test/server-get.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $name = isset($_GET['name'])? $_GET['name'] : '';
-    $age = isset($_GET['age'])? $_GET['age'] : '';
-    $response = array();
-    $response['name'] = $name;
-    $response['age'] = $age;
-    echo json_encode($response);
-?>
--- test/server.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $name = isset($_POST['name'])? $_POST['name'] : '';
-    $age = isset($_POST['age'])? $_POST['age'] : '';
-    $response = array();
-    $response['name'] = $name;
-    $response['age'] = $age;
-    echo json_encode($response);
-?>
